4dd85eca02c300e53e656766501d9c2798e9aa35
Nkemakolam Nnadi <nnnadi@unicef.org>
2019-10-01 16:19:07 -0400

    add template to the app

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello from Python!"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
